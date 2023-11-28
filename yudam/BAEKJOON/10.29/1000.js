05b0da56fa9924145b4abfa313d2261f0d735c71
youdame <112458620+youdame@users.noreply.github.com>
2023-11-28 22:01:07 +0900

    yudam : 파일 정리

---
+++ yudam/BAEKJOON/10.29/1000.js
@@ -0,0 +1,8 @@
+// const fs = require("fs");
+
+// const input = fs.readFileSync("example.txt").toString().split(' ');
+
+// const A = parseInt(input[0]);
+// const B = parseInt(input[1]);
+
+console.log(1 + 2);
