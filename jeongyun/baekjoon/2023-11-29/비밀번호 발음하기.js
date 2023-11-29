f588b9237288fa9ddbce43fc17a0d30a15ae839b
lsc58461 <lsc58461@naver.com>
2023-11-29 14:17:15 +0900

    fix: 수정

--- jeongyun/baekjoon/2023-11-29/비밀번호 발음하기.js
+++ jeongyun/baekjoon/2023-11-29/비밀번호 발음하기.js
@@ -5,22 +5,16 @@ let input = require("fs")
   .trim()
   .split("\n");
 
-for (let i = 0; i < input.length; i++) {
-  if (input[i] === "end") break;
+for (let i = 0; i < passwords.length; i++) {
+  if (passwords[i] === "end") break;
 
-  const hasVowel = input[i].match(/[aeiou]/g);
-  const hasConsecutiveVowelsOrConsonants = input[i].match(
-    /([aeiou]){3}|([^aeiou]){3}/g
-  );
-  const hasConsecutiveLetters = input[i].match(/([adf-np-z])\1/g);
+  const one = passwords[i].match(/[aeiou]/g); // 모음이 포함되어 있는지 확인
+  const two = passwords[i].match(/([aeiou]){3}|([^aeiou]){3}/g); // 연속된 모음 또는 자음이 있는지 확인
+  const three = passwords[i].match(/([^eo]|[^eo]e|[^eo]o)\1/g); // 'ee'와 'oo'를 제외한 모든 연속된 문자 확인
 
-  if (
-    hasVowel !== null &&
-    hasConsecutiveVowelsOrConsonants === null &&
-    hasConsecutiveLetters === null
-  ) {
-    console.log(`<${input[i]}> is acceptable.`);
+  if (one !== null && two === null && three === null) {
+    console.log(`<${passwords[i]}> is acceptable.`);
   } else {
-    console.log(`<${input[i]}> is not acceptable.`);
+    console.log(`<${passwords[i]}> is not acceptable.`);
   }
 }
