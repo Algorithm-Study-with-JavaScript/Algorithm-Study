eb354a742a5f4705e096d2941e41c8bfaf7cdd2b
lsc58461 <lsc58461@naver.com>
2023-12-04 00:26:36 +0900

    feat: 인프런 강의 풀이 추가

---
+++ jeongyun/inflearn/1-4 1부터 N까지의 합.js
@@ -0,0 +1,46 @@
+/*
+1부터 N까지 합 출력하기
+자연수 N이 입력되면 1부터 N까지의 합을 출력하는 프로그램을 작성하세요.
+
+▣ 입력설명
+첫 번째 줄에 20이하의 자연수 N이 입력된다..
+
+▣ 출력설명
+첫 번째 줄에 1부터 N까지의 합을 출력한다.
+
+▣ 입력예제 1 
+6
+
+▣ 출력예제 1
+21
+
+▣ 입력예제 2 
+10
+
+▣ 출력예제 2
+55
+*/
+
+const mySolution = (n) => {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+};
+
+console.log(mySolution(6));
+console.log(mySolution(10));
+
+// 강의 풀이
+function solution(n) {
+  let answer = 0;
+  for (let i = 1; i <= n; i++) {
+    answer = answer + i;
+  }
+
+  return answer;
+}
+
+console.log(solution(6));
+console.log(solution(10));
