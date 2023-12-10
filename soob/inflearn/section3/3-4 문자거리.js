e0f14cf6f9e1d14b0c752103f113bf2a88ca32be
전수빈 <soobin08011812@gmail.com>
2023-12-10 23:36:00 +0900

    Feat: section3 1,2,3,5 풀이

---
+++ soob/inflearn/section3/3-4 문자거리.js
@@ -0,0 +1,10 @@
+// 한 개의 문자열 s와 문자 t가 주어지면 문자열 s의 각 문자가 문자 t와 떨어진 최소거리를 출력하는 프로그램을 작성하세요
+
+// 입력 설명
+// 첫 번째 줄에 문자열 S와 문자 t가 주어진다.
+// 문자열과 문자는 소문자로만 주어집니다.
+
+function solution(s, t) {}
+
+let str = "teachermode";
+console.log(solution(str, "e"));
