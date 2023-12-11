b820ebec0ba6eca4452ab368004951a43f98b79b
전수빈 <soobin08011812@gmail.com>
2023-12-11 10:56:18 +0900

    Feat: section3 1 강의 풀이

--- soob/inflearn/section3/3-1 회문문자열.js
+++ soob/inflearn/section3/3-1 회문문자열.js
@@ -3,7 +3,7 @@
 // 단 회문을 검사할 때 대소문자를 구분하지 않습니다.
 
 function solution(s) {
-  for (let i = 0; i < Math.round(s.length / 2); i++) {
+  for (let i = 0; i < Math.floor(s.length / 2); i++) {
     if (s[i].toUpperCase() !== s[s.length - 1 - i].toUpperCase()) {
       return "NO";
     }
@@ -14,7 +14,25 @@ function solution(s) {
 let str = "goooG";
 console.log(solution(str));
 
-function solution1(s) {}
+function solution1(s) {
+  let answer = "YES";
+  s = s.toLowerCase();
+  let len = s.length;
+  for (let i = 0; i < Math.floor(len / 2); i++) {
+    if (s[i] !== s[len - i - 1]) return "NO";
+  }
+
+  return answer;
+}
+
+function solution2(s) {
+  let answer = "YES";
+  s = s.toLowerCase();
+  if (s.split("").reverse().join("") !== s) return "NO";
+
+  return answer;
+}
 
 let str1 = "goooG";
 console.log(solution1(str1));
+console.log(solution2(str1));
