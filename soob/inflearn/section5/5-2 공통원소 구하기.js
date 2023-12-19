25de0ea44d1dff826e64a3430ac826e68641b2c4
전수빈 <soobin08011812@gmail.com>
2023-12-19 09:05:37 +0900

    Feat: section5 2 강의 풀이

--- soob/inflearn/section5/5-2 공통원소 구하기.js
+++ soob/inflearn/section5/5-2 공통원소 구하기.js
@@ -11,3 +11,22 @@ function solution(arr1, arr2) {
 let a = [1, 3, 9, 5, 2];
 let b = [3, 2, 5, 7, 8];
 console.log(solution(a, b));
+
+// 겅의
+function solution1(arr1, arr2) {
+  let answer = [];
+  arr1.sort((a, b) => a - b);
+  arr2.sort((a, b) => a - b);
+  let p1 = (p2 = 0);
+  // 포인터값이 작은쪽을 ++하면서 배열을 모두 검색
+  while (p1 < arr1.length && p2 < arr2.length) {
+    if (arr1[p1] == arr2[p2]) {
+      answer.push(arr1[p1++]);
+      p2++;
+    } else if (arr1[p1] < arr2[p2]) p1++;
+    else p2++;
+  }
+  return answer;
+}
+
+console.log(solution1(a, b));
