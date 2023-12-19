9b28bcca40c4c4403282997c3eee9a55a6f9b696
전수빈 <soobin08011812@gmail.com>
2023-12-19 09:06:37 +0900

    Feat: section5-1 강의 풀이

--- soob/inflearn/section5/5-1 두 배열 합치기.js
+++ soob/inflearn/section5/5-1 두 배열 합치기.js
@@ -5,3 +5,20 @@ function solution(arr1, arr2) {
 let a = [1, 3, 5];
 let b = [2, 3, 6, 7, 9];
 console.log(solution(a, b));
+
+// 강의
+function solution1(arr1, arr2) {
+  let answer = [];
+  let n = arr1.length;
+  let m = arr2.length;
+  let p1 = (p2 = 0);
+  while (p1 < n && p2 < m) {
+    if (arr1[p1] <= arr2[p2]) answer.push(arr1[p1++]);
+    else answer.push(arr2[p2++]);
+  }
+  while (p1 < n) answer.push(arr1[p1++]);
+  while (p2 < m) answer.push(arr2[p2++]);
+  return answer;
+}
+
+console.log(solution1(a, b));
