241cbb823ef783cfaeba34e9b15840e05c89738a
전수빈 <soobin08011812@gmail.com>
2023-12-26 19:11:31 +0900

    Feat: section5 6-7 강의 풀이 추가

--- soob/inflearn/section5/5-6 아나그램.js
+++ soob/inflearn/section5/5-6 아나그램.js
@@ -29,3 +29,23 @@ function solution(str1, str2) {
 let a = "abaCC";
 let b = "Caaab";
 console.log(solution(a, b));
+
+// 강의
+function solution1(str1, str2) {
+  let answer = "YES";
+  let sH = new Map();
+  for (let x of str1) {
+    if (sH.has(x)) sH.set(x, sH.get(x) + 1);
+    else sH.set(x, 1);
+  }
+
+  for (let x of str2) {
+    // sH에 x가 없는 경우 || 상쇄하고 보니 더 이상 상쇄할 수 없는 경우
+    if (!sH.has(x) || sH.get(x) === 0) return "NO";
+    sH.set(x, sH.get(x) - 1);
+  }
+
+  return answer;
+}
+
+console.log(solution1(a, b));
