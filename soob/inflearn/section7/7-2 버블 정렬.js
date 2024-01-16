5189917d21d2d807cced08e7a8a38099605a1930
전수빈 <soobin08011812@gmail.com>
2024-01-16 19:28:14 +0900

    Feat: section 7 1-3 풀이

---
+++ soob/inflearn/section7/7-2 버블 정렬.js
@@ -0,0 +1,16 @@
+function solution(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+let arr = [13, 5, 11, 7, 23, 15];
+console.log(solution(arr));
