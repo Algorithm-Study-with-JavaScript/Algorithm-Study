9a23d39d8d5f8e0314fcee71f3c9658a38e6b653
전수빈 <soobin08011812@gmail.com>
2024-01-23 11:13:08 +0900

    Feat: section 7-9 강의 풀이

---
+++ soob/inflearn/section7/7-9 결혼식.js
@@ -0,0 +1,33 @@
+// 강의 풀이
+function solution(times) {
+  let answer = Number.MIN_SAFE_INTEGER;
+  let T_line = [];
+  for (let x of times) {
+    T_line.push([x[0], "s"]);
+    T_line.push([x[1], "e"]);
+  }
+
+  //시간을 기준으로 정렬, 같은 경우 e(종료 시간)가 더 먼져 오게 정렬
+  T_line.sort((a, b) => {
+    if (a[0] === b[0]) return a[1].charCodeAt() - b[1].charCodeAt();
+    else return a[0] - b[0];
+  });
+
+  let cnt = 0;
+  for (let x of T_line) {
+    if (x[1] === "s") cnt++;
+    else cnt--;
+    answer = Math.max(answer, cnt);
+  }
+
+  return answer;
+}
+
+let arr = [
+  [14, 18],
+  [12, 15],
+  [15, 20],
+  [20, 30],
+  [5, 14],
+];
+console.log(solution(arr));
